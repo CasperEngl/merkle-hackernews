b51d239be85ba472a6fb67ee7e84ad6042b09754
Casper Engelmann <me@casperengelmann.com>
2022-09-28 14:41:16 +0200

    remove preact

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,7 @@
-const withPreact = require('next-plugin-preact')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = withPreact(nextConfig)
+module.exports = nextConfig
